285efc2af0048d18affa61a4d80406665c39f11b
michael <michael.ekow.johnson@gmail.com>
2025-09-13 15:14:50 +0000

    test change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.print("Enter value for a: ");
+        double a = getInput.nextDouble();
+        System.out.print("Enter value for b: ");
+        double b = getInput.nextDouble();
+        System.out.print("Enter value for c: ");
+        double c = getInput.nextDouble();
+        System.out.print("Enter value for d: ");
+        double d = getInput.nextDouble();
+        System.out.print("Enter value for e: ");
+        double e = getInput.nextDouble();
+        System.out.print("Enter value for f: ");
+        double f = getInput.nextDouble();
+
+        double x = ( e * d - b * f) / (a * d - b * c);
+        double y = (a * f - e * c)/ (a * d - b * c);
+
+        String formattedString = String.format("x = %f, y = %f", x, y);
+
+        System.out.println(formattedString);
     }
 }
